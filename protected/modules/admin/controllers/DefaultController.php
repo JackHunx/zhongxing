93cfb67fd12e23e390517c9153b784d2976a0b77
zhongxing <king.521.love@gmail.com>
2013-06-24 20:46:47 +0800

    'login'

--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -1,10 +1,25 @@
 <?php
 
-class DefaultController extends Controller
+class DefaultController extends SBaseController
 {
+    public $layout='/layouts/admin';
+    public function init()
+    {
+        
+        
+        Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/admin/bootstrap-classic.css');
+        
+        //Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/admin/noty_theme_default.css');
+         Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/admin/charisma-app.css');
+         
+    }
 	public function actionIndex()
 	{
-	   //Yii::app()->user->checkAccess("PostView");
-		$this->render('index');
+	   if(isset($_POST['admin']))
+           { print_r($_POST);
+            exit();}
+   	   //Yii::app()->user->checkAccess("PostView");
+		$this->render('login');
 	}
+   
 }
