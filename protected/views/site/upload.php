33fbef66d48cf62bb603460f20294bbeb0eb49b9
Jack <king.521.love@gmail.com>
2013-06-28 15:42:26 +0800

    add files upload

--- protected/views/site/upload.php
+++ protected/views/site/upload.php
@@ -13,7 +13,7 @@ $this->widget('ext.EFineUploader.EFineUploader',
                        'retry'=>array('enableAuto'=>true,'preventRetryResponseProperty'=>true),
                        'chunking'=>array('enable'=>true,'partSize'=>100),//bytes
                        'callbacks'=>array(
-                                       'onComplete'=>"js:function(id, name, response){ $('.qq-upload-status-text').html(function(){return '文件上传成<a href='+response.url+'>查看</a>';});}",
+                                       'onComplete'=>"js:function(id, name, response){ $('.qq-upload-status-text').html(function(){return '文件上传成<a target=_black href='+response.url+'>查看</a>';});}",
                                         'onError'=>"js:function(id, name, errorReason){  }",
                                          ),
                        'validation'=>array(
