ea365064afb81216e2b948fb20724039a5005459
Jack <king.521.love@gmail.com>
2013-07-02 15:59:05 +0800

    add avatar extensions

---
+++ protected/extensions/avatar/AvatarUpload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 头像上传 基于flash
+ * 
+ * 
+ * 生成三个文件 big small original 文件
+ * 
+ * 
+ */ 
+ class AvatarUpload extends CWidget{
+    
+    public $version = "1.0";
+    public $id="avatarUpload";
+    public $config=array();
+    public $css = null;
+    
+ }
